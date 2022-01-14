09a0a39a62e11616202e62de141aaf7f22166890
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-01-15 00:48:38 +0530

    Add files via upload

---
+++ Arduino/Documents/Analog_Output/Analog_Out.ino
@@ -0,0 +1,19 @@
+static int LED=3;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int brightness=0;brightness<=255;brightness++)
+{
+  analogWrite(LED,brightness);
+  delay(10);
+}
+for(int brightness=255;brightness>0;brightness--)
+{
+  analogWrite(LED,brightness);
+  delay(10);
+}
+}
