9352d7386131e67864e5c124b149af7cca93a147
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-01-17 02:08:27 +0530

    Add files via upload

---
+++ Arduino/Documents/LDR/LDR.ino
@@ -0,0 +1,15 @@
+const int LDR = A0;
+int input_val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+input_val = analogRead(LDR);
+Serial.print("R value is = ");
+Serial.println(input_val);
+delay(1000);
+}
