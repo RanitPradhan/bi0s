80901c6c200cf53d41f25542c2ee61a5ebcf81fb
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-01-17 03:03:54 +0530

    Add files via upload

---
+++ Arduino/Documents/Peizo_Buzzer/Buzzer.ino
@@ -0,0 +1,13 @@
+const int buzzer = 3;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(buzzer,HIGH);
+delay(200);
+digitalWrite(buzzer,LOW);
+delay(200);
+}
